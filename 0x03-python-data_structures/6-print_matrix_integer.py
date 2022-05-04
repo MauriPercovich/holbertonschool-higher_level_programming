fb48cdd43a850793ad31d2d8c13c83e9f5d2fa27
MauriPercovich <mauriciopercovich22@gmail.com>
2022-05-03 18:42:29 -0700

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        cont = 0
+        for j in i:
+            cont += 1
+            if len(i) != cont:
+                print(f"{(j):d}", end=" ")
+            else:
+                print(f"{(j):d}", end="")
