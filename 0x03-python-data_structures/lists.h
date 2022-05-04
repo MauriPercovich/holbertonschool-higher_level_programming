2dbb2728a0bf76182addc655b91277f30eaa87e0
MauriPercovich <mauriciopercovich22@gmail.com>
2022-05-03 19:08:08 -0700

    task 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,11 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>
 
 /**
- * struct listint_s - list
- * @n: int
- * @next: next
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for project
  */
 typedef struct listint_s
 {
@@ -19,4 +21,4 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif
+#endif /* LISTS_H */
