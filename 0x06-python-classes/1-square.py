fb65204aade351547cb3a3d443b1689bc630209a
MauriPercovich <mauriciopercovich22@gmail.com>
2022-05-17 14:34:11 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """initialize"""
+        self.__size = size
