06e4ee83eb6404e2daf4b8bb72d8e0fbf0181ba4
MauriPercovich <mauriciopercovich22@gmail.com>
2022-05-17 14:25:20 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""This module writes a class Square that defines a square"""
-
-
+"""Write an empty class Square that defines a square"""
 class Square:
-    """Empty class to define a square"""
     pass
