197fcd038231a4081cfb3dc6e2c3c493aedcd405
MauriPercovich <mauriciopercovich22@gmail.com>
2022-05-17 18:01:26 -0700

    task 102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+'''comment'''
+
+
+class Square:
+    '''comment'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        '''get size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''set value for size'''
+        self.__size = value
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        '''This function return the area of square'''
+        return self.__size ** 2
+
+    def __lt__(self, other):
+        '''return comparison x<y'''
+        if not isinstance(other, Square):
+            pass
+        if self.area() < other.area():
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        '''return comparison x<=y'''
+        if not isinstance(other, Square):
+            pass
+        if self.area() <= other.area():
+            return True
+        else:
+            return False
+
+    def __eq__(self, other):
+        '''return comparison x==y'''
+        if not isinstance(other, Square):
+            pass
+        if self.area() == other.area():
+            return True
+        else:
+                return False
+
+    def __ne__(self, other):
+        '''return comparison x!=y'''
+        if not isinstance(other, Square):
+            pass
+        if self.area() != other.area():
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        '''return comparison x>y'''
+        if not isinstance(other, Square):
+            pass
+        if self.area() > other.area():
+            return True
+        else:
+            return False
+
+    def __ge__(self, other):
+        '''return comparison x>=y'''
+        if not isinstance(other, Square):
+            pass
+        if self.area() >= other.area():
+            return True
+        else:
+            return False
