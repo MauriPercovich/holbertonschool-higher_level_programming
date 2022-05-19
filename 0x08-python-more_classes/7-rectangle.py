7f88259fdc87f05e468d7bf51893ff8842732da6
MauriPercovich <mauriciopercovich22@gmail.com>
2022-05-18 18:45:38 -0700

    task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
         if self.__height is 0 or self.__width is 0:
             return ""
         pic = (str(self.print_symbol) * self.__width + "\n") * self.__height
-        return pic[:-1]
+        return pic[:-1:]
     '''return the rectangle'''
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
