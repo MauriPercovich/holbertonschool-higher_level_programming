4b7b979449e0e274a919d3f68f40b869d1c192fe
MauriPercovich <mauriciopercovich22@gmail.com>
2022-05-18 18:55:26 -0700

    task

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -3,8 +3,21 @@
 
 
 class Rectangle:
-    '''rectangle'''
-
+    """
+    represent a rectangle
+    _
+    Attributes
+    ----------
+    width : int
+        width of the rectangle
+    height : int
+        height of the rectangle
+    number_of_instances = int
+        +1 on instance
+        +1 on delete
+    print_symbol = str
+        symbol to print the rectangle
+    """
     number_of_instances = 0
     print_symbol = "#"
 
@@ -37,36 +50,35 @@ class Rectangle:
         elif height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
-    '''calculation'''
+    '''method to calculate the area of the rectangle'''
     def area(self):
         return self.__width * self.__height
-    '''calculation'''
+    '''method to calculate the perimeter of the rectangle'''
     def perimeter(self):
         if self.__height is 0 or self.__width is 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
-    '''print the rectangle'''
+    '''method print the rectangle'''
     def __str__(self):
         if self.__height is 0 or self.__width is 0:
             return ""
         pic = (str(self.print_symbol) * self.__width + "\n") * self.__height
         return pic[:-1:]
-    '''return the rectangle'''
+    '''method to return the representation of Rectangle'''
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
-    '''delete a rectangle'''
+    '''method to delete a Rectangle instance'''
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-        '''staticmethod compare rectangles'''
-        @staticmethod
-        def bigger_or_equal(rect_1, rect_2):
-            if type(rect_1) is not Rectangle:
-                raise TypeError("rect_1 must be an instance of Rectangle")
-            if type(rect_2) is not Rectangle:
-                raise TypeError("rect_2 must be an instance of Rectangle")
-            if rect_1.area() >= rect_2.area():
-                return rect_1
-            else:
-                return rect_2
-
+    ''' staticmethod to compare to Rectangle instances '''
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
