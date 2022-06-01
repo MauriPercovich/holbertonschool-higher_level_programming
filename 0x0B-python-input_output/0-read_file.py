01da931ee9397b2aa6d9e5a76de3ce7df058b9af
MauriPercovich <mauriciopercovich22@gmail.com>
2022-05-31 19:16:35 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"reads a file"
+"""function that reads a text file"""
 
 
 def read_file(filename=""):
-    """
-    reads file
-    """
-    with open(filename, mode="r", encoding="utf-8") as leer:
-        print(leer.read())
+    """function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
