04668b598118270294aeab25cb2a055870f0bb2d
MauriPercovich <mauriciopercovich22@gmail.com>
2022-09-15 13:03:21 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* File System Object */
+var fs = require('fs');
+
+try {
+  const data = fs.readFileSync(process.argv[2], 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
